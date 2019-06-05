22734c42917f42d34fd174db97dd091bcc0e5250
ZoranWong <904110231@qq.com>
2019-06-05 17:29:15 +0800

    服务订单修改

--- database/migrations/2018_12_23_042632_create_order_items_table.php
+++ database/migrations/2018_12_23_042632_create_order_items_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateOrderItemsTable extends Migration
 {
@@ -15,18 +16,18 @@ class CreateOrderItemsTable extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('order_id');
-            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
-            $table->unsignedInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->unsignedInteger('product_sku_id');
-            $table->foreign('product_sku_id')->references('id')->on('product_skus')->onDelete('cascade');
-            $table->unsignedInteger('amount');
-            $table->decimal('price', 10, 2);
-            $table->unsignedInteger('rating')->nullable();
-            $table->text('review')->nullable();
-            $table->timestamp('reviewed_at')->nullable();
+            $table->unsignedInteger('order_id')->comment('订单ID');
+            $table->unsignedInteger('product_id')->comment('产品ID');
+            $table->json('product')->comment('产品快照');
+            $table->unsignedInteger('install_fee')->default(0)->comment('安装费用');
+            $table->unsignedInteger('other_fee')->default(0)->comment('其他费用');
+            $table->unsignedTinyInteger('status')->default(0)->comment('订单状态');
+            $table->unsignedTinyInteger('type')->default(0)->comment('订单类型');
+            $table->unsignedInteger('master_id')->default(0)->comment('雇佣师傅ID');
+            $table->softDeletes();
+            $table->timestamps();
         });
+        DB::statement('ALTER TABLE `order_items` COMMENT "订单详情表"');
     }
 
     /**
