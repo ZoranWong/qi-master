cba1cd2e9ef19e77d2c97a96d25a80dc925208e6
ZoranWong <904110231@qq.com>
2019-06-06 09:26:14 +0800

    修改数据文件

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('mobile');
+            $table->string('name')->comment('用户名');
+            $table->string('email')->default('')->comment('邮箱');
+            $table->string('mobile')->default('')->comment('手机');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
