8b6fc82aad85bea4ee0917193dd61fd0ad588011
ZoranWong <904110231@qq.com>
2019-06-10 14:52:33 +0800

    添加货币

---
+++ app/Models/Traits/CurrencyUnitTrait.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangzaron
+ * Date: 2019/6/10
+ * Time: 2:43 PM
+ */
+
+namespace App\Models\Traits;
+
+
+trait CurrencyUnitTrait
+{
+    protected $currencyColumns = [];
+
+    public function setAttribute($key, $value)
+    {
+        if (array_search($key, $this->currencyColumns)) {
+            $value *= CURRENCY_UNIT_CONVERT_NUM;
+        }
+        parent::setAttribute($key, $value);
+    }
+
+    public function getAttribute($key)
+    {
+        $value = parent::getAttribute($key);
+        if(array_search($key, $this->currencyColumns)){
+            $value /= CURRENCY_UNIT_CONVERT_NUM;
+        }
+        return $value;
+    }
+}
--- app/Utils/PatternDict.php
+++ app/Utils/PatternDict.php
@@ -1,3 +1,4 @@
 <?php
 
 define('PAGE_SIZE', 15);
+define('CURRENCY_UNIT_CONVERT_NUM', 100);
