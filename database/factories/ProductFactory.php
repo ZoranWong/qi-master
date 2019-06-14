ccd4793538886c2d2261922a87bf514c2e355c3f
ZoranWong <904110231@qq.com>
2019-06-14 11:44:49 +0800

    fix

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,16 +4,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
     $image = $faker->randomElement([
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/7kG1HekGK6.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/1B3n0ATKrn.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/r3BNRe4zXG.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/C0bVuKB2nt.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/82Wf2sg8gM.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/nIvBAQO5Pj.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/XrtIwzrxj7.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/uYEHCJ1oRp.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
+        "https://fupo.jp/wp-content/uploads/2019/04/MG_7849-c2.jpg",
+        "http://img.fsjiaju.com/Product/2014/1208/20141208144843253u204104.jpg",
+        "https://tgi1.jia.com/118/571/18571330.jpg",
+        "https://tgi12.jia.com/118/571/18571304.jpg",
+        "http://decomyplace.com/img/blog/150616_clei_0.jpg",
+        "http://decomyplace.com/img/blog/150616_clei_1.jpg",
+        "http://decomyplace.com/img/blog/150616_clei_5.jpg",
+        "http://decomyplace.com/img/blog/150616_clei_10.jpg"
     ]);
 
     return [
--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -14,13 +14,6 @@ class ProductsSeeder extends Seeder
     public function run()
     {
         // 创建 30 个商品
-        $products = factory(Product::class, 30)->create();
-        /** @var Product $product */
-        foreach ($products as $product) {
-            // 创建 3 个 SKU，并且每个 SKU 的 `product_id` 字段都设为当前循环的商品 id
-            $skus = factory(ProductSku::class)->create(['product_id' => $product->id]);
-            // 找出价格最低的 SKU 价格，把商品价格设置为该价格
-//            $product->update(['price' => $skus->min('price')]);
-        }
+        factory(Product::class, 30)->create();
     }
 }
