3f512d810a35d9e99fd0b34ea0ab88698285fa37
wong1989 <skyandsunning@163.com>
2019-06-20 02:35:51 +0800

    fix

--- resources/views/web/login.blade.php
+++ resources/views/web/login.blade.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="/web/css/styles.css"/>
     <script type="text/javascript" src="/web/js/jquery-3.3.1.js"></script>
     <script type="text/javascript" src="/web/plugin/layui/layui.js"></script>
+    <style>
+        .layui-form-item{
+            margin-top: 0 !important;
+            margin-bottom: 0 !important;
+        }
+    </style>
 </head>
 
 
@@ -31,12 +37,20 @@
                 <span class="icon">用户登录</span>
                 <span class="fr">还没有账号？ <a href="register">立即注册&gt;&gt;</a></span>
             </div>
-            <input type="tel" id="phone" class="login-phone" maxlength="11" placeholder="请输入您的手机号">
-            <input type="password" id="password" class="password" placeholder="请输入您的密码">
-            <button onclick="login()">立即登录</button>
-            <div class="forget">
-                <a href="forget/password">忘记密码</a>
-            </div>
+            <form class="layui-form">
+                <div class="layui-form-item">
+                    <input type="tel" id="phone" class="login-phone" maxlength="11" placeholder="请输入您的手机号" lay-verify="required">
+                </div>
+                <div class="layui-form-item">
+                    <input type="password" id="password" class="password" placeholder="请输入您的密码" lay-verify="required">
+                </div>
+                <div class="layui-form-item">
+                    <button lay-submit class="layui-btn" lay-filter='*'>立即登录</button>
+                    <div class="forget">
+                        <a href="forget/password">忘记密码</a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 
@@ -44,9 +58,32 @@
 <!--content end-->
 </body>
 <script>
-    $loginUrl = '{{$loginRoute}}';
-    function login() {
+    layui.use('form', function(){
+        var form = layui.form;
+        form.on('submit(*)',function(data){
+            console.log('--------', data);
+            return false;
+        });
 
+        //各种基于事件的操作，下面会有进一步介绍
+    });
+    let loginUrl = '{{$loginRoute}}';
+
+    function login() {
+        let mobile = $('#phone').val();
+        let password = $('#password').val();
+        if (!mobile || !password) {
+            return;
+        }
+        let data = {
+            'mobile': mobile,
+            'password': password
+        };
+        $.post(loginUrl, data).done((res) => {
+
+        }).fail((error) => {
+
+        });
     }
 </script>
 </html>
