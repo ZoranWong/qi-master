24894627fb39e2f0ce5d0fa0778848e7ceb654d6
katherine <1134052988@qq.com>
2019-06-24 15:12:13 +0800

    improve

--- database/migrations/2019_06_24_062806_add_column_to_refund_order_table.php
+++ database/migrations/2019_06_24_062806_add_column_to_refund_order_table.php
@@ -18,9 +18,9 @@ class AddColumnToRefundOrderTable extends Migration
             $table->unsignedTinyInteger('refund_mode')->comment('退款服务');
             $table->unsignedTinyInteger('refund_method')->comment('退款方式');
             $table->unsignedTinyInteger('has_customer')->default(0)->comment('是否客服介入');
-            $table->json('audit')->comment('服务商处理结果');
+            $table->json('audit')->nullable()->comment('服务商处理结果');
             $table->json('arbitration')->nullable()->comment('仲裁结果');
-            $table->unsignedTinyInteger('apply_status')->comment('服务商处理结果');
+            $table->unsignedTinyInteger('apply_status')->default(0)->comment('申请状态');
         });
     }
 
