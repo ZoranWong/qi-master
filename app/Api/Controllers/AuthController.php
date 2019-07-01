7d0ef8875477aca358e9f47c462441b5f5677b88
katherine <1134052988@qq.com>
2019-07-02 01:33:05 +0800

    improve

--- app/Api/Controllers/AuthController.php
+++ app/Api/Controllers/AuthController.php
@@ -84,7 +84,7 @@ class AuthController extends Controller
     protected function responseWithToken($token)
     {
         return response()->json([
-            'user' => $this->auth->user(),
+            'user' => auth()->user(),
             'token' => 'bearer ' . $token
         ]);
     }
