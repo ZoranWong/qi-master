c61e902e8d4c089bd7824ba4ff47b760b5d6a35c
katherine <1134052988@qq.com>
2019-07-03 15:20:36 +0800

    fix

--- app/Http/Requests/UserUpdatePasswordRequest.php
+++ app/Http/Requests/UserUpdatePasswordRequest.php
@@ -39,7 +39,6 @@ class UserUpdatePasswordRequest extends FormRequest
             || $uri === api_route('masters.change_wallet_password')
         ) {
             $rules['old_password'] = 'required|string|min:6';
-            \Log::info('更新密码路由:', ['request_uri' => $uri]);
             unset($rules['mobile']);
         } else if ($uri === api_route('user.set.wallet_password')
             || $uri === api_route('user.reset.wallet_password')
@@ -48,6 +47,7 @@ class UserUpdatePasswordRequest extends FormRequest
             unset($rules['mobile']);
         }
 
+        \Log::info('更新密码路由:', ['request_uri' => $uri, 'uri' => api_route('masters.change_password')]);
         return $rules;
     }
 }
