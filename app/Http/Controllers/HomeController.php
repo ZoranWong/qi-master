3fb05cc0989d64982c1dab760431c2480a568aca
ZoranWong <904110231@qq.com>
2019-07-04 13:38:47 +0800

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,7 @@ class HomeController extends Controller
                 'currentMenu' => ''
             ]);
         }
+
         $orders = $user->orders()->with(['items', 'offerOrders'])->offset(0)->limit(10)->get();
         $view->with('user', $user)->with('orders', $orders);
         return $view;
