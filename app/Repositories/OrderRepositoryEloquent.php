5e5848d33b9abdb5eacc03b286b9c9722fc2ae3f
ZoranWong <904110231@qq.com>
2019-08-26 15:23:34 +0800

    fix

--- app/Repositories/OrderRepositoryEloquent.php
+++ app/Repositories/OrderRepositoryEloquent.php
@@ -134,14 +134,12 @@ class OrderRepositoryEloquent extends BaseRepository implements OrderRepository
         $paginator = $this->with(['serviceType', 'classification'])
             ->scopeQuery(function (Builder $query) use ($master) {
             return $query->where(function ($query) {
-                $query->whereRaw('status & ? = ? ',  [Order::ORDER_WAIT_HIRE, Order::ORDER_WAIT_HIRE])
-                    ->orWhere('status', '=', Order::ORDER_WAIT_OFFER);
+                $query->where('status', '<', Order::ORDER_EMPLOYED);
             })->where(function ($query) {
                 $query->whereDoesntHave('offerOrders', function ($query) {
                     $query->where('master_id', auth()->user()->id);
                 })->orWhereDoesntHave('offerOrders');
-            })->where('status', '<=', Order::ORDER_WAIT_HIRE)
-                ->join('master_services', function (JoinClause $join) use ($master) {
+            })->join('master_services', function (JoinClause $join) use ($master) {
                     $join->on('orders.region_code', '=', 'master_services.region_code')
                         ->where('master_services.master_id', '=', $master->id);
                 })
