c21761349e4843bde709835992ae3eb59a25fb28
ZoranWong <904110231@qq.com>
2019-10-19 23:03:06 +0800

    fix

--- app/Transformers/ArticleTransformer.php
+++ app/Transformers/ArticleTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers;
 
+use Illuminate\Support\Facades\Storage;
 use League\Fractal\TransformerAbstract;
 use App\Models\Article;
 
@@ -24,7 +25,7 @@ class ArticleTransformer extends TransformerAbstract
         return [
             'id'         => (int) $model->id,
             'publish_at' => $model->publishAt->format('Y-m-d'),
-            'cover_image' => $model->coverUrl,
+            'cover_image' => Storage::disk(config('admin.upload.disk'))->url($model->coverUrl),
             'title' => $model->title,
             'content' => $model->content,
             'sort' => $model->sort,
