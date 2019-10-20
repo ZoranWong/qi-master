59793b0edbdc655b2d741c20db1949542255bef0
ZoranWong <904110231@qq.com>
2019-10-20 13:25:04 +0800

    fix

--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -87,11 +87,20 @@
                 }
             }
         });
-
+        let url = "{{route('user.register')}}}";
         form.on('submit(*)', function(data){
             console.log(data.elem) //被执行事件的元素DOM对象，一般为button对象
             console.log(data.form) //被执行提交的form对象，一般在存在form标签时才会返回
             console.log(data.field) //当前容器的全部表单字段，名值对形式：{name: value}
+            $.ajax({
+                url: url,
+                method: 'POST',
+                data: data,
+                dataType: 'json',
+                success(res) {
+                    console.log(res);
+                }
+            });
             return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
         });
         //各种基于事件的操作，下面会有进一步介绍
