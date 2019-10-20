f97ec7c621f9981ac6a61fd9e8e7b30c0bcc1efd
ZoranWong <904110231@qq.com>
2019-10-20 13:01:45 +0800

    fix

--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -55,7 +55,7 @@
                     <a href="">网站注册协议</a>
                 </div>
                 <div class="layui-form-item">
-                    <button class="register-btn" onclick="register()">立即注册</button>
+                    <button class="register-btn" lay-submit lay-filter="*">立即注册</button>
                 </div>
             </div>
             <div class="line"><img src="/web/image/line.png"></div>
@@ -69,5 +69,37 @@
 </div>
 <!--content end-->
 </body>
+<script>
+    layui.use('form', function(){
+        var form = layui.form;
+        form.verify({
+            username: function(value, item){ //value：表单的值、item：表单的DOM对象
+                if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)){
+                    return '用户名不能有特殊字符';
+                }
+                if(/(^\_)|(\__)|(\_+$)/.test(value)){
+                    return '用户名首尾不能出现下划线\'_\'';
+                }
+                if(/^\d+\d+\d$/.test(value)){
+                    return '用户名不能全为数字';
+                }
+            }
 
+            //我们既支持上述函数式的方式，也支持下述数组的形式
+            //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+            ,pass: [
+                /^[\S]{6,12}$/
+                ,'密码必须6到12位，且不能出现空格'
+            ]
+        });
+
+        form.on('submit(*)', function(data){
+            console.log(data.elem) //被执行事件的元素DOM对象，一般为button对象
+            console.log(data.form) //被执行提交的form对象，一般在存在form标签时才会返回
+            console.log(data.field) //当前容器的全部表单字段，名值对形式：{name: value}
+            return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+        });
+        //各种基于事件的操作，下面会有进一步介绍
+    });
+</script>
 </html>
